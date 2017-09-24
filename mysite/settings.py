9dd48c21f71b37134dcf4e23ed4ecb46cf317688
Kayleigh Raine <kayleighraine@gmail.com>
2017-09-24 10:04:29 +0200

    allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3=#2-pf!pddw@xqy9)vtseh^2@9aas0pdn@kp-9-(7idv42k=i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'OmnomnOmnigul.pythonanywhere.com']
 
 
 # Application definition
